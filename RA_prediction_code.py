364173dc427c3086b6fcbae642bd4637b3d6e693
Sanghyun Kim <sanghyunkim@Sanghyuns-MacBook-Pro.local>
2021-01-25 21:04:05 +0900

    Code edit

--- RA_prediction_code.py
+++ RA_prediction_code.py
@@ -5,6 +5,9 @@ import seaborn as sns
 from sklearn import preprocessing, linear_model, metrics
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import StandardScaler
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 import statsmodels.api as sm
 from scipy import stats
@@ -34,7 +37,7 @@ pitching_2010 = pd.read_csv('/Users/sanghyunkim/Desktop/Data Science Project/MLB
 # merge datasets
 pitching_dfs = [pitching_2019, pitching_2018, pitching_2017, pitching_2016, pitching_2015,
                 pitching_2014, pitching_2013, pitching_2012, pitching_2011, pitching_2010]
-pitching_df = reduce(lambda left,right: pd.merge(left, right, how='outer'), pitching_dfs)
+pitching_df = reduce(lambda x, y: pd.merge(x, y, how='outer'), pitching_dfs)
 print(pitching_df.head().to_string())
 
 # drop unnecessary columns
@@ -43,6 +46,27 @@ pitching_df.drop(['#'], axis=1, inplace=True)
 # rename specific column names
 pitching_df.rename(columns={'R': 'RA'}, inplace=True)
 
+# check data types
+print(pitching_df.dtypes)
+
+# categorical data
+obj_cols = list(pitching_df.select_dtypes(include='object').columns)
+print(pitching_df[obj_cols].head())
+
+# remove commas in 'IP' and 'PA' data
+cols = ['IP', 'PA']
+pitching_df[cols] = pitching_df[cols].replace(',', '', regex=True)
+print(pitching_df[cols].head())
+
+# convert 'IP' and 'PA' data types from categorical data into numeric data
+pitching_df[cols] = pitching_df[cols].apply(pd.to_numeric)
+
+# drop categorical variables as we're not going to use them to predict 'RA'
+pitching_df = pitching_df.select_dtypes(exclude='object')
+
+# check new data types
+print(pitching_df.dtypes)
+
 # check missing values
 print("Total Number of Missing Values in Pitching Data:")
 print(pitching_df.isnull().sum())
@@ -50,23 +74,48 @@ print(pitching_df.isnull().sum())
 # check duplicates
 print("Total Number of Duplicates in Pitching Data: {}".format(pitching_df.duplicated().sum()))
 
-
-### 2. EDA (Exploratory Data Analysis) ###
+# pitching statistics descriptive summary
 print("------- Pitching Data Descriptive Summary -------")
 print(pitching_df.describe().to_string())
+# according to the descriptive summaries above, some data features have 0 values (invalid values)
+# treat these invalid values as missing values
 
-## RA data distribution
+# check invalid '0' values
+print('------- Number of 0 values in each Data Variable -------')
+print(pitching_df[pitching_df == 0].count())
+
+# looking at data ranges, the 2 '0' values in 'FIP_MINUS_ERA' data feature seems valid
+# therefore, don't treat those 2 '0' values as invalid values
+
+# treat all the '0' values as missing values except 'FIP_MINUS_ERA' data
+pitching_df = pitching_df.drop(['FIP_MINUS_ERA'], axis=1).replace(0, np.nan)
+print(pitching_df.isnull().sum())
+
+# Imputation
+imputer = IterativeImputer(random_state=0).fit_transform(pitching_df)
+
+pitching_df = pd.DataFrame(data=imputer, columns=pitching_df.columns)
+print(pitching_df.isnull().sum())
+
+# check imputed data decriptive summaries
+print(pitching_df.describe().to_string())
+
+
+
+### 2. EDA (Exploratory Data Analysis) ###
+
+# dependent variable, 'RA' EDA
+fig, axes = plt.subplots(1, 2, figsize=(20, 8))
+
+sns.histplot(pitching_df['RA'], kde=True, ax=axes[0])
+axes[0].set_title('Team RA Histogram')
+
+axes[1] = stats.probplot(pitching_df['RA'], plot=plt)
+plt.title('Team RA Q-Q Plot')
+
+plt.show()
+
 print('------- Team Runs Allowed Distribution -------')
-sns.displot(pitching_df['RA'], kde=True)
-plt.title('Team RA Distribution')
-plt.tight_layout()
-plt.show()
-
-stats.probplot(pitching_df['RA'], plot=plt)
-plt.title('Team RA Probability Plot')
-plt.tight_layout()
-plt.show()
-
 print('Mean RA: {}'.format(pitching_df['RA'].mean()))
 print('Median RA: {}'.format(pitching_df['RA'].median()))
 print('RA Standard Deviation: {}'.format(pitching_df['RA'].std()))
@@ -76,167 +125,279 @@ print('RA Kurtosis: {}'.format(pitching_df['RA'].kurt()))
 print('#Conclusion: Team RA distribution is approximately normal')
 
 # yearly changes in RA
-yearly_ra = pd.concat([pitching_df['YEAR'], pitching_df['RA']], axis=1)
-
 fig, ax = plt.subplots(figsize=(10, 10))
 
-sns.boxplot(x='YEAR', y='RA', data=yearly_ra, ax=ax)
-ax.set(title='Yearly Changes in Team Runs Allowed')
+sns.boxplot(pitching_df['YEAR'], pitching_df['RA'], ax=ax)
+ax.set_title('Yearly Changes in Team Runs Allowed')
 
 plt.show()
 
-# pitching data correlation heatmap
-corrP = pitching_df.corr()
-sns.heatmap(corrP, square=True, cmap='Blues_r')
-plt.title('Pitching Statistics Correlation Heatmap')
-plt.tight_layout()
+# correlation matrix
+corr = pitching_df.corr()
+fig, ax = plt.subplots(figsize=(10, 10))
+
+sns.heatmap(corr, square=True, cmap='YlGnBu', vmax=1, vmin=-1, ax=ax)
+ax.set_title('Correlation Matrix')
 
 plt.show()
 
-# Top10 RA correlation heatmap
-top10_corrP_cols = corrP.nlargest(10, 'RA')['RA'].index
-top10_corrP = np.corrcoef(pitching_df[top10_corrP_cols].values.T)
-sns.set(font_scale=1.25)
-sns.heatmap(top10_corrP, cmap='Blues_r', cbar=True, annot=True, square=True, annot_kws={'size':10},
-            xticklabels=top10_corrP_cols.values, yticklabels=top10_corrP_cols.values)
-plt.title("Top10 Pitching Statistics Correlated with RA")
-plt.tight_layout()
+print(corr.to_string())
+
+# drop variables that have lower correlations with 'RA' than 0.65
+corr = abs(pitching_df.corr())
+corr_df = corr['RA'].to_frame(name='Correlation with RA').T
+corr_cols = corr_df.columns
+
+corr_df.drop(columns=corr_cols[(corr_df < 0.65).any()], inplace=True)
+print(corr_df.to_string())
+
+selected_cols = list(corr_df.columns)
+pitching_df = pitching_df[selected_cols]
+print(pitching_df.head().to_string())
+
+# new correlation matrix for selected data features
+corr = pitching_df.corr()
+fig, ax = plt.subplots(figsize=(10, 10))
+
+sns.heatmap(corr, square=True, annot=True, annot_kws={'size':10}, cmap='YlGnBu',
+            vmax=1, vmin=-1, center=0,
+            xticklabels=corr.columns, yticklabels=corr.columns, ax=ax)
+ax.set_title('Correlation Matrix')
 
 plt.show()
 
-# pitching statistics visualization
-high_corrP = pitching_df.reindex(columns=top10_corrP_cols)
-x_axesP = list(high_corrP.columns[1:10])
 
-## histograms
-fig, axes = plt.subplots(3, 3, figsize=(20, 20))
+# independent variables EDA
+# histograms
+cols = pitching_df.drop(['RA'], axis=1)
+fig, axes = plt.subplots(4, 3, figsize=(20, 20))
 
-for col, ax in zip(x_axesP, axes.flatten()[:9]):
-    sns.histplot(x=pitching_df[col], color='blue', kde=True, ax=ax)
+for col, ax in zip(cols, axes.flatten()[:11]):
+    sns.histplot(pitching_df[col], kde=True, color='blue', ax=ax)
     ax.set_title('Team {} Histogram'.format(col))
-    ax.set_xlabel('Team {}'.format(col))
-    ax.set_ylabel('Count')
 
 plt.show()
 
-## scatter plots
-fig = plt.figure(figsize=(20, 20))
-cScatter = 1
+# Q-Q plots
+fig, axes = plt.subplots(4, 3, figsize=(21, 21))
 
-for col in x_axesP:
-    plt.subplot(3, 3, cScatter)
-    plt.title("Correlation between Team {} and RA".format(col))
-    plt.xlabel(col)
-    plt.ylabel("RA")
-    plt.scatter(x=col, y="RA", data=high_corrP, c=high_corrP[col])
-    cScatter = cScatter + 1
+for col, ax in zip(cols, axes.flatten()[:11]):
+    stats.probplot(pitching_df[col], plot=ax)
+    ax.set_title('{} Q-Q Plot'.format(col))
 
 plt.show()
 
-# multicollinearity
-data = pitching_df[top10_corrP_cols].drop(['RA', 'DRA_START'], axis=1)
-cols = list(data.columns)
+# scatter plots
+fig, axes = plt.subplots(4, 3, figsize=(20, 20))
 
-x = pitching_df[cols]
-x = sm.add_constant(x)
-y = pitching_df['RA']
-
-lm = sm.OLS(y, x)
-result = lm.fit()
-print(result.summary())
-
-# VIF
-vif = pd.DataFrame()
-vif['Feature'] = lm.exog_names
-vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
-print(vif.loc[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
-
-# feature selection
-print('------- Pitching Statistics Correlation -------')
-print(pitching_df.corr().to_string())
-feature_selec = pitching_df[cols].drop(['DRA-', 'FIP', 'ERA', 'DRA', 'BB9', 'cFIP'], axis=1)
-cols = list(feature_selec.columns)
-
-x = pitching_df[cols]
-x = sm.add_constant(x)
-y = pitching_df['RA']
-
-lm = sm.OLS(y, x)
-result = lm.fit()
-print(result.summary())
-
-vif = pd.DataFrame()
-vif['Feature'] = lm.exog_names
-vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
-print(vif.loc[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
-
-# feature scaling
-# check the scale of 'WHIP' and 'HR9'
-fig, ax = plt.subplots(figsize=(10, 10))
-sns.kdeplot(pitching_df['WHIP'], ax=ax, label='WHIP')
-sns.kdeplot(pitching_df['HR9'], ax=ax, label='HR9')
-ax.set_title('Before RobustScaler')
-ax.set_xlabel('Data Scale')
-ax.legend(loc='upper right')
+for col, ax in zip(cols, axes.flatten()[:11]):
+    sns.regplot(x=col, y='RA', data=pitching_df,
+                scatter_kws={'color':'navy'}, line_kws={'color':'red'}, ax=ax)
+    ax.set_title('Correlation between Team {} and RA'.format(col))
 
 plt.show()
 
-# scale data
-RB_scaler = preprocessing.RobustScaler()
-scaled_df = RB_scaler.fit_transform(pitching_df[['WHIP', 'HR9']])
-scaled_df = pd.DataFrame(scaled_df, columns=['WHIP', 'HR9'])
 
-# check scaled 'WHIP' and 'HR9' KDE plot
-fig, ax = plt.subplots(figsize=(10, 10))
-cols = list(scaled_df.columns)
 
-for col in cols:
-    sns.kdeplot(scaled_df[col], ax=ax, label=col)
-    ax.set_title('After RobustScaler')
+### 3. Feature Scaling ###
+
+print('------- Pitching Statistics Descriptive Summary -------')
+print(pitching_df.describe().to_string())
+# since data ranges vary considerably scale them using StandardScaler
+
+# StandardScaler
+df = pitching_df.drop(['RA'], axis=1)
+cols = list(df.columns)
+
+std_scaler = StandardScaler()
+scaled_data = std_scaler.fit_transform(df)
+scaled_df = pd.DataFrame(scaled_data, columns=cols)
+
+# KDE plot after Scaling
+scaled_cols = list(scaled_df.columns)
+fig, ax = plt.subplots(figsize=(8, 8))
+
+for col in scaled_cols:
+    sns.kdeplot(scaled_df[col], label=col, ax=ax)
+    ax.set_title('After StandardScaler')
     ax.set_xlabel('Data Scale')
-    ax.legend(loc=1)
+    plt.legend(loc=1)
 
 plt.show()
 
-# multiple linear regression
-data = pd.concat([scaled_df[['WHIP', 'HR9']], pitching_df['RA']], axis=1)
-x = data[['WHIP', 'HR9']]
+
+
+### 4. Multiple Linear Regression with feature selection
+df = pd.concat([pitching_df['RA'], scaled_df], axis=1)
+x = df.drop(['RA'], axis=1)
 x = sm.add_constant(x)
-y = data['RA']
+y = df['RA']
 
 lm = sm.OLS(y, x)
 result = lm.fit()
 print(result.summary())
 
-x = data[['WHIP', 'HR9']]
-y = data['RA']
+# Variance Inflation Factor (VIF)
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+# exclude 'DRA-'
+x = df.drop(['RA', 'DRA-'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
 
-lm = linear_model.LinearRegression().fit(x_train, y_train)
-y_predicted = lm.predict(x_test)
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
 
-print('------- Intercept -------')
-print(lm.intercept_)
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
 
-print('------- Coefficient -------')
-print(lm.coef_)
+# exclude 'cFIP_START' and 'DRA_START'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
 
-print('------- RMSE -------')
-mse = metrics.mean_squared_error(y_test, y_predicted)
-print(sqrt(mse))
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
 
-print('------- R-squared -------')
-print(metrics.r2_score(y_test, y_predicted))
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
 
-# K-Fold Cross-Validation
-model = LinearRegression()
-cv_r2 = cross_val_score(model, x, y, scoring='r2', cv=10)
-cv_mse = cross_val_score(model, x, y, scoring='neg_mean_squared_error', cv=10)
-cv_rmse = np.sqrt(-1 * cv_mse)
-print('------- 10-Fold Cross-Validation -------')
-print('Mean R-squared: {}'.format(cv_r2.mean()))
-print('Mean RMSE: {}'.format(cv_rmse.mean()))
+# exclude 'PWARP'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# exclude 'FIP'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# exclude 'ERA'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP', 'ERA'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# exclude 'DRA'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP', 'ERA', 'DRA'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# exclude 'cFIP'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP', 'ERA', 'DRA', 'cFIP'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# include 'DRA' again
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP', 'ERA', 'cFIP'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# exclude 'DRA' and include 'ERA' again
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP', 'DRA', 'cFIP'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+# exclude 'ERA' and 'PA'
+x = df.drop(['RA', 'DRA-', 'cFIP_START', 'DRA_START', 'PWARP', 'FIP', 'DRA', 'cFIP', 'ERA', 'PA'], axis=1)
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+x = df[['WHIP', 'HR9', 'PA']]
+x = sm.add_constant(x)
+y = df['RA']
+
+lm = sm.OLS(y, x)
+result = lm.fit()
+print(result.summary())
+
+vif = pd.DataFrame()
+vif['Feature'] = lm.exog_names
+vif['VIF'] = [variance_inflation_factor(lm.exog, i) for i in range(lm.exog.shape[1])]
+print(vif[vif['Feature'] != 'const'].sort_values('VIF', ascending=False))
+
+
+
 
 
 # x = np.array(pitching_df['ERA']).reshape(-1, 1)
